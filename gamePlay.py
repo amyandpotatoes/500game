394dd17867436647b3f835a52239c8636e7ffde9
Eamon <u6351631@anu.edu.au>
2019-12-28 20:58:25 +1100

    trying to complete game init and some cleaning'

--- gamePlay.py
+++ gamePlay.py
@@ -4,15 +4,21 @@ from itertools import chain
 from random import shuffle
 
 class Game:
-    def __init__(self):
-        # type can be "single" or "first to 500"
-        self.type = ""
+    def __init__(self, type="500", player_count=4, teams={}):
+        # type can be "single" or "500"
+        if type == "500" or type == "single":
+            self.type = type
+        else:
+            raise Exception("invald type parameter for game")
         # player_count can be 3, 4, 5 or 6
-        self.player_count = 0
+        self.player_count = player_count
         # teams is a dictionary of teams and their members
-        self.teams = dict()
+        self.teams = teams
         # players is a list of players in the order they play
-        self.players = []
+        if not teams:
+            teams[0] = [0,2]
+            teams[1] = [1,3]
+        self.players = range(player_count)
 
     def initGame(self):
         raise NotImplementedError
@@ -53,7 +59,7 @@ class Round:
         red_cards = None
         black_cards = None
 
-        while not (red_cards and black_cards):
+        while not red_cards or not black_cards:
             if self.player_count == 3:
                 # need 33 cards total, so 16 number cards, red 7-10, black 7-10
                 red_cards = list(chain(range(7, 11), range(14, 18)))
@@ -71,7 +77,7 @@ class Round:
                 red_cards = list(range(2, 18))
                 black_cards = list(range(3, 18))
             else:
-                print("Please choose a number of players between 3 and 6.")
+                raise Exception("Please choose a number of players between 3 and 6.")
 
         # create notrumps suit and cards (joker and no-trumps bids)
         notrumps = Suit("NT", 5)
