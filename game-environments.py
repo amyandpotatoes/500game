f43920159746316dbea1629d446c86297e0cb546
asanson01 <u5805193@anu.edu.au>
2021-01-26 14:35:29 +1100

    basic scaffolding

---
+++ game-environments.py
@@ -0,0 +1,34 @@
+class CustomEnvironment(Environment):
+
+    def __init__(self, num_locations, num_cards):
+        super().__init__()
+        self.num_locations = num_locations
+        self.num_cards = num_cards
+
+    def states(self):
+        # num_locations is the number of places card can be - number of players + 1
+        # true if the card is in that location
+        return dict(type='bool', shape=(self.num_locations, self.num_cards))
+
+    def actions(self):
+        return dict(type='int', num_values=self.num_cards)
+
+    # Optional: should only be defined if environment has a natural fixed
+    # maximum episode length; otherwise specify maximum number of training
+    # timesteps via Environment.create(..., max_episode_timesteps=???)
+    def max_episode_timesteps(self):
+        return super().max_episode_timesteps()
+
+    # Optional additional steps to close environment
+    def close(self):
+        super().close()
+
+    def reset(self):
+        state = np.random.random(size=(8,))
+        return state
+
+    def execute(self, actions):
+        next_state = np.random.random(size=(8,))
+        terminal = np.random.random() < 0.5
+        reward = np.random.random()
+        return next_state, terminal, reward
---
+++ main.py
@@ -0,0 +1,84 @@
+# just using template from FLight example
+# will make it work for cards
+# scaffold, not a working python file at this point
+
+
+def run(environment, agent, n_episodes, max_step_per_episode, test=False):
+    """
+    Train agent for n_episodes
+    """
+    environment.FlightModel.max_step_per_episode = max_step_per_episode
+    # Loop over episodes
+    for i in range(n_episodes):
+        # Initialize episode
+        episode_length = 0
+        states = environment.reset()
+        internals = agent.initial_internals()
+        terminal = False
+        while not terminal:
+            # Run episode
+            episode_length += 1
+            actions = agent.act(states=states)
+            states, terminal, reward = environment.execute(actions=actions)
+            agent.observe(terminal=terminal, reward=reward)
+
+    # return the 'score' for recording purposes
+    return environment.FlightModel.Pos[0]
+
+
+def runner(
+    environment,
+    agent,
+    max_step_per_episode,
+    n_episodes,
+    n_episodes_test=1,
+    combination=1,
+):
+    # Train agent
+    result_vec = [] #initialize the result list
+    for i in range(round(n_episodes / 100)): #Divide the number of episodes into batches of 100 episodes
+        if result_vec:
+            print("batch", i, "Best result", result_vec[-1]) #Show the results for the current batch
+        # Train Agent for 100 episode
+        run(environment, agent, 100, max_step_per_episode)
+        # Test Agent for this batch
+        test_results = run(
+                environment,
+                agent,
+                n_episodes_test,
+                max_step_per_episode,
+                test=True
+            )
+        # Append the results for this batch
+        result_vec.append(test_results)
+    # Plot the evolution of the agent over the batches
+    plot_multiple(
+        Series=[result_vec],
+        labels = ["Reward"],
+        xlabel = "episodes",
+        ylabel = "Reward",
+        title = "Reward vs episodes",
+        save_fig=True,
+        path="env",
+        folder=str(combination),
+        time=False,
+    )
+    #Terminate the agent and the environment
+    agent.close()
+    environment.close()
+
+def main:
+    # Instantiane our environment
+    environment = PlaneEnvironment()
+    # Instantiate a Tensorforce agent
+    agent = Agent.create(agent="ppo",environment=environment)
+
+    # Call runner
+    runner(
+        environment,
+        agent,
+        max_step_per_episode=1000,
+        n_episodes=10000)
+
+if __name__ == '__main__':
+    main()
